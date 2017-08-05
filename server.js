07190d023f388c50a470d5ce2b6a8b9ea26dd19b
Ajin RL <30746206+rlajin750@users.noreply.github.com>
2017-08-05 19:14:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
     app.get('/article-one', function (req, res) {
-         res.send(createTemplate(articleone));
+         res.send(createTeplate(articleone));
 });
 
  app.get('/article-two', function (req, res) {
